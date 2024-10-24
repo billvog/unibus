62b51f11901dce0e003c81114c904996a8eebe31
billvog <billvog04@gmail.com>
2024-10-24 12:42:53 +0300

    api: run migrations on startup

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,26 @@
 import "@api/utils/axios";
+import path from "path";
+
+import { migrate } from "drizzle-orm/node-postgres/migrator";
 import Express from "express";
 
 import { addTrpc } from "@api/app-router";
+import { db } from "@api/db";
 import { env } from "@api/env";
 import { IS_PROD } from "@api/utils/constants";
 import { registerCronJobs } from "@api/utils/register-cron-jobs";
 
-function main() {
+async function main() {
   const app = Express();
 
+  // Only run on production
   if (IS_PROD) {
+    // Migrate the database
+    await migrate(db, {
+      migrationsFolder: path.join(__dirname, "../drizzle"),
+    });
+
+    // Use the trust proxy middleware
     app.set("trust proxy", 1);
   }
 
@@ -22,4 +33,4 @@ function main() {
   });
 }
 
-main();
+void main();
