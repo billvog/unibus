8f8446829ac447748a9757bb000e46cd7564f963
billvog <billvog04@gmail.com>
2024-11-03 19:26:01 +0200

    web: whoami reworked and sitemap

--- web/src/app/whoami/page.tsx
+++ web/src/app/whoami/page.tsx
@@ -1,17 +1,61 @@
-import React from "react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@web/components/ui/card";
+import { Metadata } from "next";
+
+const description =
+  "Τι είναι το Unibus και ποιός είναι ο δημιουργός του; Μάθε περισσότερα για τον Vasilis Voyiadjis.";
+
+export const metadata: Metadata = {
+  title: "Ποιός είμαι ― Unibus",
+  description,
+  twitter: {
+    description,
+  },
+  openGraph: {
+    description,
+  },
+};
 
 const Page = () => {
   return (
-    <div className="flex h-dvh w-screen flex-col items-center justify-center gap-4">
-      <div className="text-2xl">Hello, World!</div>
-      <a
-        href="https://billvog.com/?ref=unibus.gr"
-        target="_blank"
-        className="font-bold text-blue-500 underline-offset-4 after:ml-1 hover:underline"
-      >
-        billvog.com
-      </a>
-    </div>
+    <Card className="w-full max-w-xl">
+      <CardHeader>
+        <CardTitle>Hello, World!</CardTitle>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-6 text-balance">
+        <p>
+          Είμαι ο Βασίλης, ένας προγραμματιστής και φοιτητής από την Ελλάδα, και
+          αυτό είναι το Unibus.
+        </p>
+        <p>
+          Το Unibus είναι μια εφαρμογή που φτιάχνω για να βοηθήσω τους ανθρώπους
+          να μετακινούνται στην πόλη της Λαμίας (όπου φοιτώ), χρησιμοποιώντας τη
+          δημόσια συγκοινωνία, και αργότερα όλο τον 🌎.
+        </p>
+        <div className="space-y-2">
+          <p>Μπορείς να με βρεις στα παρακάτω:</p>
+          <ul className="list-inside list-disc space-y-2">
+            <li>
+              <a href="https://billvog.com/?ref=unibus" target="_blank">
+                στο website μου 🧑‍💻
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/vasilis-voyiadjis/"
+                target="_black"
+              >
+                στο linkedin 🔗
+              </a>
+            </li>
+          </ul>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
 
