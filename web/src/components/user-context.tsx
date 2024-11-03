b3db625bc24e119ca2335fc35b090f89a90222d8
billvog <billvog04@gmail.com>
2024-11-03 15:42:16 +0200

    web: indentify users in posthog

--- web/src/components/user-context.tsx
+++ web/src/components/user-context.tsx
@@ -1,5 +1,6 @@
 import type { DbUser } from "@api/types/models";
-import { createContext, useContext, useEffect, useState } from "react";
+import posthog from "posthog-js";
+import React, { createContext, useContext, useState } from "react";
 
 import { trpc } from "@web/lib/trpc";
 
@@ -12,11 +13,29 @@ const UserContext = createContext<UserContextType | null>(null);
 export function UserProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<UserContextType["user"]>(null);
 
-  const { data: userData } = trpc.user.me.useQuery();
+  const { data: userData, isLoading } = trpc.user.me.useQuery();
 
-  useEffect(() => {
+  React.useEffect(() => {
     setUser(userData ?? null);
   }, [userData]);
+
+  // Identify user in PostHog, using their unique ID
+  React.useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+
+    if (!user) {
+      posthog.reset();
+    }
+
+    if (user) {
+      posthog.identify(user.id, {
+        email: user.email,
+        name: user.name,
+      });
+    }
+  }, [user, isLoading]);
 
   return (
     <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
