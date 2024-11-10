fa2c69e6fe3b0f478d055699a8ce05cff2913330
billvog <billvog04@gmail.com>
2024-11-10 22:06:36 +0200

    api: put sentry dsn to env var

--- api/src/lib/sentry.ts
+++ api/src/lib/sentry.ts
@@ -1,8 +1,9 @@
+import { env } from "@api/env";
 import * as Sentry from "@sentry/node";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 Sentry.init({
-  dsn: "https://7e28a727e9dbd301f24f521dbaee703f@o4508275271401472.ingest.de.sentry.io/4508275378618448",
+  dsn: env.SENTRY_DSN,
   integrations: [nodeProfilingIntegration()],
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
