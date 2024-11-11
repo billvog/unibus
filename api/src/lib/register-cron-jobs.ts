dfe3c7433950eb240260d83d05324bdd62c81250
billvog <billvog04@gmail.com>
2024-11-11 20:38:55 +0200

    feat(api): monitor cron jobs with sentry

--- api/src/lib/register-cron-jobs.ts
+++ api/src/lib/register-cron-jobs.ts
@@ -1,18 +1,21 @@
 import cron from "node-cron";
+import * as Sentry from "@sentry/node";
 
 import { fetchStaticCitybusFeed } from "@api/lib/citybus/fetch-static-feed";
 import { generateCitybusToken } from "@api/lib/citybus/token";
 
+const cronWithCheckIn = Sentry.cron.instrumentNodeCron(cron);
+
 export const registerCronJobs = () => {
   // Generate a new Citybus token every day at midnight
-  cron.schedule("0 0 * * *", () => void generateCitybusToken(), {
+  cronWithCheckIn.schedule("0 0 * * *", () => void generateCitybusToken(), {
     runOnInit: true,
     name: "generate-citybus-token",
     timezone: "Europe/Athens",
   });
 
   // Refetch the static Citybus feed every month at midnight
-  cron.schedule("0 0 1 * *", () => void fetchStaticCitybusFeed(), {
+  cronWithCheckIn.schedule("0 0 1 * *", () => void fetchStaticCitybusFeed(), {
     name: "fetch-static-citybus-feed",
     timezone: "Europe/Athens",
     scheduled: false, // disable for now
