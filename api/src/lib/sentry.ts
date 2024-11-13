9aef6c3a197e88feb8698e2453eb84dbca001c01
billvog <billvog04@gmail.com>
2024-11-13 12:16:24 +0200

    only enable sentry in production

--- api/src/lib/sentry.ts
+++ api/src/lib/sentry.ts
@@ -6,6 +6,7 @@ Sentry.init({
   dsn: env.SENTRY_DSN,
   environment: env.NODE_ENV,
   release: env.SENTRY_RELEASE,
+  enabled: env.NODE_ENV === "production",
   integrations: [nodeProfilingIntegration()],
   // Tracing
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
